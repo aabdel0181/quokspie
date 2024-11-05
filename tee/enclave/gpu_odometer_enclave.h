10fdae6aa5dd154febb2edef45ae015aaad18735
tarunyaa <tarunyaa@seas.upenn.edu>
2024-11-05 05:19:58 +0000

    tee structure

---
+++ tee/enclave/gpu_odometer_enclave.h
@@ -0,0 +1,10 @@
+#ifndef GPU_ODOMETER_ENCLAVE_H_
+#define GPU_ODOMETER_ENCLAVE_H_
+
+#include "asylo/enclave.pb.h"
+#include "asylo/trusted_application.h"
+#include <nvml.h>
+
+class GpuOdometerEnclave : public asylo::TrustedApplication {
+
+};
