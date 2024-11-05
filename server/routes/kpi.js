610d03c1b2f6506026c97a1493b717b5e7dbcba3
aabdel0181 <ahmedabdellahhoco@gmail.com>
2024-11-05 16:56:14 -0500

    no longer fried?

--- server/routes/kpi.js
+++ server/routes/kpi.js
@@ -4,15 +4,15 @@ import KPI from "../models/KPI.js";
 const router = express.Router();
 
 router.get("/kpis", async (req, res) => {
-    console.log("Route handler reached");
-    try {
-        const kpis = await KPI.find();
-        console.log("KPIs fetched:", kpis);
-        res.status(200).json(kpis);
-    } catch (error) {
-        console.error("Error fetching KPIs:", error);
-        res.status(500).json({ message: error.message });
-    }
+  console.log("Route handler reached");
+  try {
+    const kpis = await KPI.find();
+    console.log("KPIs fetched:", kpis);
+    res.status(200).json(kpis);
+  } catch (error) {
+    console.log("Error fetching KPIs:", error);
+    res.status(500).json({ message: error.message });
+  }
 });
 
 export default router;
