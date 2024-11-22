4398effb8a375d25da8aece15196da52de285901
aabdel0181 <68669398+aabdel0181@users.noreply.github.com>
2024-11-21 21:07:01 -0500

    fix deb installer

--- release-daemon/install.sh
+++ release-daemon/install.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-# Compile cpp 
-g++ -o quakspie main.cpp -lnvidia-ml -lpthread
-
-# Move the executable to /usr/local/bin
-sudo mv quakspie /usr/local/bin/
-
-# Move the service file to systemd directory
-sudo mv quakspie.service /etc/systemd/system/
-
 # Reload systemd
 sudo systemctl daemon-reload
 
-# Enable and start our service :D 
+# Enable and start the service
 sudo systemctl enable quakspie.service
 sudo systemctl start quakspie.service
 
-echo "Quakspie has successfully been installed and started!"   
+echo "Quakspie GPU Monitor has been installed and started."
