bf6185d91c93e9a27a10adc81993214ac53fcd47
enouafo11 <enouafo@seas.upenn.edu>
2024-12-02 13:59:11 -0500

    Fixed text issues on small screens

--- frontend/src/scenes/home/Row1.tsx
+++ frontend/src/scenes/home/Row1.tsx
@@ -1,67 +1,55 @@
 import DashboardBox from '../../components/DashboardBox';
-import { Box } from '@mui/material';
+import { Box, useMediaQuery } from '@mui/material';
 import BoxHeader from '../../components/BoxHeader';
 
 
-
-
 const Row1 = () => {
+    const isSmallScreen = useMediaQuery("(max-width: 1200px)");
 
     return (
         <>
-            {/* Dropdown menu */}
             <DashboardBox gridArea="a" sx={{ boxShadow: 'none' }}>
                 <BoxHeader
                 />
-                <Box 
+                 <Box 
                     display="flex" 
                     flexDirection="column"
                     justifyContent="center" 
                     alignItems="center" 
-                    fontSize="1rem"
                     height="100%"
                     style={{ marginTop: '-60px', position: 'relative', zIndex: 1 }}
                 >
-                    <h1 style={{ 
-                        fontSize: "2rem", 
-                        color: "white", 
-                        marginTop: "4rem",
-                        textAlign: "center" }}
-                        >
-                            Manage, Resell, and Optimize your GPUs
-                    </h1>
-                    <h2 style={{ 
-                        fontSize: "7rem", 
-                        color: "white", 
-                        marginTop: "-2rem",
-                        textAlign: "center" }}
-                        >
-                            The First GPU Odometer
-                    </h2>
-                </Box>
-                <Box 
-                    display="flex" 
-                    flexDirection="column"
-                    justifyContent="center" 
-                    alignItems="center" 
-                    height="100%"
-                    style={{ marginTop: '20px', position: 'relative', zIndex: 1 }}
-                >
                     <div style={{ 
-                        fontSize: "1.5rem", 
+                        fontSize: isSmallScreen ? "1rem" : "2rem", 
                         color: "white", 
-                        marginTop: "-47rem",
+                        marginTop: isSmallScreen ? "7rem" : "12rem",
                         textAlign: "center" }}
-                        >
+                    >
+                            Manage, Sell, and Optimize Your GPUs
+                    </div>
+                    <h1 style={{ 
+                        fontSize: isSmallScreen ? "3rem" : "6rem", 
+                        color: "white", 
+                        marginTop: "0rem",
+                        textAlign: "center" }}
+                    >
+                        The First GPU Odometer
+                    </h1>
+                    <div style={{ 
+                        fontSize: isSmallScreen ? "0.75rem" : "1.5rem", 
+                        color: "white", 
+                        marginTop: isSmallScreen ? "0rem" : "0rem",
+                        textAlign: "center" }}
+                    >
                             Number of Users
                     </div>
                     <h2 style={{ 
-                        fontSize: "4rem", 
+                        fontSize: isSmallScreen ? "1.5em" : "3rem", 
                         color: "white", 
-                        marginTop: "-1rem",
-                        textAlign: "center" }}
-                        >
-                            1,000,000
+                        marginTop: "0rem",
+                        textAlign: "center",
+                    }}>
+                        1,000,000
                     </h2>
                 </Box>
             </DashboardBox>
